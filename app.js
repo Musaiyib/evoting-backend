f04dac06b188088821d206b7bf8562eb75b99207
Musaiyib <myusoro@gmail.com>
2022-10-31 13:13:24 +0100

    candidate model/candidate controller is modified

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
-import * as dotenv from 'dotenv';
+import * as dotenv from "dotenv";
 dotenv.config();
 import express, { json } from "express";
 // import colors from "colors";
 import { Database } from "./config.js";
 import cors from "cors";
 import userRoute from "./routes/userRoute.js";
-import paymentRoute from "./routes/paymentRoute.js";
+// import paymentRoute from "./routes/paymentRoute.js";
 import candidateRoute from "./routes/candidateRoute.js";
-import voteRoute from "./routes/voteRoute.js"
+import voteRoute from "./routes/voteRoute.js";
 import cookieParser from "cookie-parser";
 
 const app = express();
@@ -17,20 +17,19 @@ app.use(cookieParser());
 
 app.use("/api/users", userRoute);
 app.use("/api/candidate", candidateRoute);
-app.use("/api/payments", paymentRoute);
-app.use('/api/vote', voteRoute)
+// app.use("/api/payments", paymentRoute);
+app.use("/api/vote", voteRoute);
 
 const port = process.env.PORT || 4000;
 (async () => {
   try {
-    const db = new Database
-    await db.connect()
+    const db = new Database();
+    await db.connect();
     // start the app engine
     app.listen(port, () => {
-      console.log("NODE SERVER STARTED: https://127.0.0.1:" + port)
-    })
-
+      console.log("NODE SERVER STARTED: https://127.0.0.1:" + port);
+    });
   } catch (error) {
-    console.error(error)
+    console.error(error);
   }
 })();
